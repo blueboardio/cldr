75c10a44117b04085d1e8973f3d5d6be939f0cda
Olivier Mengué <olivier@blueboard.io>
2018-11-08 12:33:42 +0100

    country: more emoji checks

--- country/code_test.go
+++ country/code_test.go
@@ -28,6 +28,8 @@ import (
 	"flag"
 	"fmt"
 	"testing"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/blueboardio/cldr/country"
 )
@@ -81,6 +83,20 @@ func TestCode(t *testing.T) {
 
 func TestEmoji(t *testing.T) {
 	for code, info := range country.Countries {
-		t.Log(country.Code(code), country.Code(code).Emoji(), info.Name)
+		emoji := country.Code(code).Emoji()
+		t.Log(country.Code(code), emoji, info.Name)
+		if len(emoji) != 8 {
+			t.Errorf("%s: incorrect length %d", code, len(emoji))
+		}
+		for {
+			r, size := utf8.DecodeRuneInString(emoji)
+			if !unicode.Is(unicode.Regional_Indicator, r) {
+				t.Errorf("%s: unexpected rune %d", code, r)
+			}
+			emoji = emoji[size:] // next
+			if emoji == "" {
+				break
+			}
+		}
 	}
 }
