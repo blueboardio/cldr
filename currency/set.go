2b619093e84d122ce1ef425dee1a60737059b39b
Olivier Mengué <olivier@blueboard.io>
2019-01-09 12:19:54 +0100

    currency: add type Set
    Semantic is a bit different from country.Set. Semantic of country.Set
    will be fixed in incoming 2.0.0.

---
+++ currency/set.go
@@ -0,0 +1,325 @@
+/*
+Copyright (c) 2019 BlueBoard SAS.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package currency
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+// Set represents a set of currency codes.
+//
+// The JSON representation (MarshalJSON) is an array of strings.
+// The text representation (MarshalText) is codes separated by comma.
+// The string representation (.String) is the text representation but with "*" if the set is empty.
+type Set []Code
+
+var ErrInvalidSet = errors.New("invalid currencies set")
+
+// Len implements sort.Interface.
+func (cs Set) Len() int {
+	return len(cs)
+}
+
+// Less implements sort.Interface.
+func (cs Set) Less(i, j int) bool {
+	return cs[i] < cs[j]
+}
+
+// Swap implements sort.Interface.
+func (cs Set) Swap(i, j int) {
+	cs[i], cs[j] = cs[j], cs[i]
+}
+
+// String implements fmt.Stringer.
+func (cs Set) String() string {
+	if cs == nil {
+		return "*"
+	}
+	b, _ := cs.MarshalText()
+	return string(b)
+}
+
+// Set implements flag.Value.
+func (cs *Set) Set(src string) error {
+	if src == "*" {
+		*cs = nil
+		return nil
+	}
+	return cs.UnmarshalText([]byte(src))
+}
+
+// MarshalText implements encoding.TextMarshaler.
+func (cs Set) MarshalText() ([]byte, error) {
+	b := make([]byte, 0, 4*len(cs))
+	for _, c := range cs {
+		b = append(b, c...)
+		b = append(b, ',')
+	}
+	if len(b) > 0 {
+		b = b[:len(b)-1] // Remove the last comma
+	}
+	return b, nil
+}
+
+// UnmarshalText implements encoding.TextUnmarshaler.
+func (cs *Set) UnmarshalText(b []byte) error {
+	switch len(b) {
+	case 0:
+		*cs = nil
+		return nil
+	case 1:
+		if b[0] == '*' {
+			*cs = nil
+			return nil
+		}
+		return ErrInvalidSet
+	case 3:
+		c := Code(b)
+		if !c.IsValid() {
+			return ErrInvalidSet
+		}
+		*cs = Set{c}
+	default:
+		if (len(b)-3)%4 != 0 {
+			return ErrInvalidSet
+		}
+		set := make(Set, 1+(len(b)-3)/4)
+		set[0] = Code(b[0:3])
+		if !set[0].IsValid() {
+			return ErrInvalidSet
+		}
+		for i := 1; i < len(set); i++ {
+			if b[4*i-1] != ',' {
+				return ErrInvalidSet
+			}
+			set[i] = Code(b[4*i : 4*i+3])
+			if !set[i].IsValid() {
+				return ErrInvalidSet
+			}
+		}
+		*cs = set
+	}
+	return nil
+}
+
+// MarshalJSON implements encoding/json.Marshaler.
+func (cs Set) MarshalJSON() ([]byte, error) {
+	if cs == nil {
+		return []byte("null"), nil
+	}
+	b := make([]byte, len(cs)*(1+3+1+1)-1+2)
+	b[0] = '['
+	p := 1
+	for _, c := range cs {
+		b[p] = '"'
+		b[p+1] = c[0]
+		b[p+2] = c[1]
+		b[p+3] = c[2]
+		b[p+4] = '"'
+		b[p+4] = ','
+		p += 6
+	}
+	b[len(b)-1] = ']' // Overwrite the last comma
+	return b, nil
+}
+
+// UnmarshalJSON implements encoding/json.Unmarshaler.
+func (cs *Set) UnmarshalJSON(b []byte) error {
+	var raw []Code // anonymous
+	err := json.Unmarshal(b, &raw)
+	if err != nil {
+		if err == ErrInvalidCurrencyCode {
+			var raw2 []string
+			_ = json.Unmarshal(b, &raw2)
+			for _, c := range raw2 {
+				if !Code(c).IsValid() {
+					return fmt.Errorf("%q: invalid currency code", c)
+				}
+			}
+		}
+		return err
+	}
+	*cs = raw
+	return nil
+}
+
+// Value() implements database/sql/driver.Valuer.
+func (cs Set) Value() (driver.Value, error) {
+	if cs == nil {
+		return nil, nil
+	}
+	return cs.MarshalText()
+}
+
+func (cs *Set) Scan(src interface{}) error {
+	var b []byte
+	switch src := src.(type) {
+	case nil:
+		*cs = nil
+		return nil
+	case []byte:
+		if len(src) == 0 {
+			*cs = nil
+			return nil
+		}
+		b = src
+	case string:
+		if len(src) == 0 {
+			*cs = nil
+			return nil
+		}
+		b = []byte(src)
+	default:
+		return fmt.Errorf("invalid type %T for type %T", src, *cs)
+	}
+	if len(b) < 2 {
+		return fmt.Errorf("invalid value %q for type %T", b, *cs)
+	}
+	// Allow a legacy verbose representation as JSON
+	if b[0] == '[' {
+		return cs.UnmarshalJSON(b)
+	}
+	return cs.UnmarshalText(b)
+}
+
+// Contains returns true if cs contains c.
+func (cs Set) Contains(c Code) bool {
+	for _, cc := range cs {
+		if cc == c {
+			return true
+		}
+	}
+	return false
+}
+
+// MatchesAny returns true if the set is empty.
+func (cs Set) MatchesAny() bool {
+	return len(cs) == 0
+}
+
+// Matches returns true if the set is empty (matches anything) or contains c.
+//
+// Same as Contains except for the empty set case which return true for any currency.
+func (cs Set) Matches(c Code) bool {
+	if cs.MatchesAny() {
+		return true
+	}
+	return cs.Contains(c)
+}
+
+// Remove removes from cs the intersection of the two sets.
+// If exclude is empty nothing is removed.
+// Order is preserved.
+func (cs *Set) Remove(exclude Set) {
+	if len(*cs) == 0 || len(exclude) == 0 {
+		return
+	}
+	s := *cs
+	removed := 0
+	for i, cc := range s {
+		if exclude.Contains(cc) {
+			removed++
+		} else {
+			if removed > 0 { // shift
+				s[i-removed] = s[i]
+			}
+		}
+	}
+	if removed == len(s) {
+		*cs = nil
+	} else if removed > 0 {
+		*cs = s[:len(s)-removed]
+	}
+}
+
+// Filter removes countries that are not Matched by filter.
+//
+// The filter is assumed to not contain duplicates.
+// An empty filter is a no-op (matches anything).
+// Order is preserved.
+func (cs *Set) Filter(filter Set) {
+	if cs.MatchesAny() || filter.MatchesAny() {
+		return
+	}
+	s := *cs
+	removed := 0
+Next:
+	for i, cc := range s {
+		for j, f := range filter {
+			if cc == f {
+				if removed > 0 { // shift
+					s[i-removed] = s[i]
+				}
+				if j == 0 {
+					filter = filter[1:]
+				}
+				continue Next
+			}
+		}
+		removed++
+	}
+	if removed == len(s) {
+		*cs = nil
+	} else if removed > 0 {
+		*cs = s[:len(s)-removed]
+	}
+}
+
+// HasDuplicates checks if cs contains duplicates.
+func (cs Set) HasDuplicates() bool {
+	l := len(cs)
+	if l == 0 {
+		return false
+	}
+	for i := 1; i < l; i++ {
+		if cs[:i].Contains(cs[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+// RemoveDuplicates remove duplicates.
+// Order is preserved. In case of a duplicate the first one is kept.
+func (cs *Set) RemoveDuplicates() {
+	l := len(*cs)
+	if l <= 1 {
+		return
+	}
+	removed := 0
+	for i := 1; i < l; i++ {
+		if (*cs)[:i-removed].Contains((*cs)[i]) {
+			removed++
+		} else {
+			if removed > 0 {
+				(*cs)[i-removed] = (*cs)[i]
+			}
+		}
+	}
+	if removed > 0 {
+		*cs = (*cs)[:len(*cs)-removed]
+	}
+}
---
+++ currency/set_test.go
@@ -0,0 +1,158 @@
+/*
+Copyright (c) 2019 BlueBoard SAS.
+
+Permission is hereby granted, EURee of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to GBPal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be incluGBPd in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIGBPD "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINEURINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLGBPRS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING EUROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER GBPALINGS IN
+THE SOFTWARE.
+*/
+
+package currency_test
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/blueboardio/cldr/currency"
+)
+
+var _ interface {
+	fmt.Stringer
+	driver.Value
+	encoding.TextMarshaler
+	json.Marshaler
+	sort.Interface
+} = currency.Set(nil)
+
+var _ interface {
+	flag.Value
+	sql.Scanner
+	encoding.TextUnmarshaler
+	json.Unmarshaler
+} = (*currency.Set)(nil)
+
+func parseSet(str string) (cs currency.Set) {
+	err := cs.Set(str)
+	if err != nil {
+		panic(fmt.Errorf("can't parse %q", str))
+	}
+	return
+}
+
+func TestSetRemove(t *testing.T) {
+	for _, test := range []struct {
+		orig     string
+		remove   string
+		expected string // Result of orig.Remove(remove)
+	}{
+		{orig: "", remove: "", expected: ""},
+		{orig: "", remove: "", expected: ""},
+		{orig: "", remove: "EUR", expected: ""},
+		{orig: "", remove: "EUR", expected: ""},
+		{orig: "EUR,GBP", remove: "", expected: "EUR,GBP"},
+		{orig: "EUR,GBP", remove: "", expected: "EUR,GBP"},
+		{orig: "EUR,GBP", remove: "EUR", expected: "GBP"},
+		{orig: "EUR,GBP", remove: "GBP", expected: "EUR"},
+		{orig: "EUR,GBP,USD", remove: "GBP", expected: "EUR,USD"},
+		{orig: "EUR,GBP,USD", remove: "EUR,GBP", expected: "USD"},
+		{orig: "EUR,GBP,USD", remove: "EUR,USD", expected: "GBP"},
+		{orig: "EUR,GBP,USD", remove: "GBP,USD", expected: "EUR"},
+	} {
+		orig, remove, expected := parseSet(test.orig), parseSet(test.remove), parseSet(test.expected)
+
+		result := parseSet(test.orig)
+		result.Remove(remove)
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("%v.Remove(%v): got %v, expected %v", orig, remove, result, expected)
+		}
+	}
+}
+
+func TestSetFilter(t *testing.T) {
+	for _, test := range []struct {
+		orig     string
+		filter   string
+		filtered string // Result of orig.Filter(remove)
+	}{
+		{orig: "*", filter: "*", filtered: "*"},
+		{orig: "*", filter: "*", filtered: "*"},
+		{orig: "*", filter: "EUR", filtered: "*"},
+		{orig: "*", filter: "EUR", filtered: "*"},
+		{orig: "EUR,GBP", filter: "", filtered: "EUR,GBP"},
+		{orig: "EUR,GBP", filter: "", filtered: "EUR,GBP"},
+		{orig: "EUR,GBP", filter: "EUR", filtered: "EUR"},
+		{orig: "EUR,GBP", filter: "GBP", filtered: "GBP"},
+		{orig: "EUR,GBP,USD", filter: "", filtered: "EUR,GBP,USD"},
+		{orig: "EUR,GBP,USD", filter: "GBP", filtered: "GBP"},
+		{orig: "EUR,GBP,USD", filter: "EUR,GBP", filtered: "EUR,GBP"},
+		{orig: "EUR,GBP,USD", filter: "EUR,USD", filtered: "EUR,USD"},
+		{orig: "EUR,GBP,USD", filter: "USD,GBP", filtered: "GBP,USD"},
+		{orig: "EUR,GBP,USD", filter: "EUR,GBP,USD", filtered: "EUR,GBP,USD"},
+		{orig: "EUR,GBP,USD", filter: "USD,GBP,EUR", filtered: "EUR,GBP,USD"},
+	} {
+		orig, filter, filtered := parseSet(test.orig), parseSet(test.filter), parseSet(test.filtered)
+
+		result := parseSet(test.orig)
+		result.Filter(filter)
+		if !reflect.DeepEqual(result, filtered) {
+			t.Errorf("%v.Filter(%v): got %v, expected %v", orig, filter, result, filtered)
+		}
+	}
+}
+
+func TestSetRemoveDuplicates(t *testing.T) {
+	for _, test := range []struct {
+		orig     string
+		expected string
+	}{
+		{orig: "", expected: ""},
+		{orig: "EUR", expected: "EUR"},
+		{orig: "EUR,EUR", expected: "EUR"},
+		{orig: "EUR,GBP", expected: "EUR,GBP"},
+		{orig: "EUR,EUR,GBP", expected: "EUR,GBP"},
+		{orig: "EUR,GBP,GBP", expected: "EUR,GBP"},
+		{orig: "EUR,EUR,GBP,GBP", expected: "EUR,GBP"},
+		{orig: "EUR,GBP,EUR,GBP", expected: "EUR,GBP"},
+		{orig: "EUR,GBP,GBP,EUR", expected: "EUR,GBP"},
+		{orig: "USD,USD,EUR,GBP", expected: "USD,EUR,GBP"},
+		{orig: "USD,EUR,USD,GBP", expected: "USD,EUR,GBP"},
+		{orig: "USD,EUR,GBP,USD", expected: "USD,EUR,GBP"},
+		{orig: "USD,EUR,USD,USD,GBP", expected: "USD,EUR,GBP"},
+		{orig: "USD,EUR,USD,GBP,USD", expected: "USD,EUR,GBP"},
+		{orig: "USD,USD,EUR,USD,GBP,USD", expected: "USD,EUR,GBP"},
+		{orig: "USD,USD,EUR,USD,USD,GBP,USD,USD", expected: "USD,EUR,GBP"},
+	} {
+		orig, expected := parseSet(test.orig), parseSet(test.expected)
+
+		l := len(orig)
+		result := parseSet(test.orig)
+		hasDupes := result.HasDuplicates()
+		result.RemoveDuplicates()
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("%v.RemoveDuplicates(): got %v, expected %v", orig, result, expected)
+		}
+		if hasDupes != (len(result) < l) {
+			t.Errorf("%v.HasDuplicates(): fail", orig)
+		}
+	}
+}
